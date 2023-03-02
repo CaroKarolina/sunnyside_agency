4769ca702ab544a1a24c3cee4ec267600e94b5c3
Karolina Karczewska <karolcia_yahoo94@wp.pl>
2023-03-02 18:47:26 +0100

    Add pseudo class :focus

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-const menuBarsIcon = document.querySelector('.menu__bars')
-menuBarsIcon.addEventListener('click', () => {
-  menuBarsIcon.parentElement.classList.toggle('collapsible--expanded')
-})
+const menuBarsIcon = document.querySelector(".menu__bars");
+const menuItemsEl = menuBarsIcon.nextElementSibling;
+const menuAppEl = document.createElement("div");
+
+menuBarsIcon.addEventListener("click", () => {
+  if (!menuItemsEl.parentElement.classList.contains("collapsible--expanded"))
+    menuItemsEl.append(menuAppEl);
+  menuItemsEl.parentElement.classList.toggle("collapsible--expanded");
+});
+
+Array.from(menuItemsEl.children).forEach((el) =>
+  el.addEventListener("click", () => {
+    el.classList.add('menu__active')
+    console.log("Klik");
+  })
+);
